21706fdaa05f90fbcdfb0fb324823d59a9620f48
rishalbinfaisal <rishalbinfaisal0@gmail.com>
2026-06-16 12:24:14 +0530

    add

---
+++ jsss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Simple script that prints a minimal HTML page to stdout."""
+
+def main():
+    html = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">">
+    <title>sss</title>
+</head>
+<body>
+    sss
+</body>
+</html>
+"""
+    print(html)
+
+
+if __name__ == "__main__":
+    main()
